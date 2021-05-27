ee6137a4d2c44260a13e00b1e981de1b6a547fab
Steffen Müthing <steffen.muething@posteo.de>
2021-05-27 02:36:28 +0200

    Hook and inhibit the Upgrade_implementation() method of the splitter
    This method causes crashes for some reason when upgrading a standard
    splitter to an Auto Splitter, so just disable it.

--- Plugins/AutoSplitters/Source/AutoSplitters/Private/AutoSplittersModule.cpp
+++ Plugins/AutoSplitters/Source/AutoSplitters/Private/AutoSplittersModule.cpp
@@ -2,7 +2,8 @@
 
 #include "Patching/NativeHookManager.h"
 
-#include "Buildables/FGBuildableAttachmentSplitter.h"
+//#include "FGDismantleInterface.h"
+//#include "Buildables/FGBuildableAttachmentSplitter.h"
 #include "Buildables/MFGBuildableAutoSplitter.h"
 
 #include "AutoSplittersLog.h"
@@ -14,25 +15,140 @@ void FAutoSplittersModule::StartupModule()
 
 #if UE_BUILD_SHIPPING
 
-	void* Instance = GetMutableDefault<AFGBuildableAttachmentSplitter>();
+	//void* Instance = static_cast<int*>(GetMutableDefault<AFGBuildableAttachmentSplitter>());
+
+
+	auto PreUpgradeHook = [](auto& Call, UObject* self)
+	{
+
+		UE_LOG(LogAutoSplitters,Display,TEXT("Entered hook for IFGDismantleInterface::Execute_PreUpgrade()"));
 	
-	UE_LOG(LogAutoSplitters,Display,TEXT("Instance ptr is %d"),Instance);
+		AFGBuildableAttachmentSplitter* Source = Cast<AFGBuildableAttachmentSplitter>(self);
+		if (!Source)
+		{
+			UE_LOG(LogAutoSplitters,Display,TEXT("self is not an AFGBuildableAttachmentSplitter, bailing out"));
+			return;
+		}
 
-	/*
-	SUBSCRIBE_METHOD_VIRTUAL(AFGBuildableAttachmentSplitter::Upgrade_Implementation,Instance,[](auto& Call, AFGBuildableAttachmentSplitter* self, AActor* newActor)
-	{
-		UE_LOG(LogAutoSplitters,Display,TEXT("Hook was hit"));
-		Call(self,newActor);
-	});
-	*/
+		UE_LOG(LogAutoSplitters,Display,TEXT("Upgrading splitter to Auto Splitter"));
+	
+		// WARNING
+		// this is a REALLY ugly hack, but should be safe as long as we ONLY access the otherwise off-limits
+		// protected members mInputs and mOutputs of the source through this reference, and as long as there
+		// is no funny virtual inheritance business going on, which there shouldn't be.
+		AMFGBuildableAutoSplitter& DowncastedSource = *static_cast<AMFGBuildableAutoSplitter*>(Source);
 
-	/*
-	SUBSCRIBE_METHOD_VIRTUAL(AFGBuildableAttachmentSplitter::FillDistributionTable,Instance,[](auto& Call, AFGBuildableAttachmentSplitter* self, float dt)
+		// let's get the lay of the land first
+		/*
+		for (int32 i = 0 ; i < DowncastedSource.mInputs.Num() ; ++i)
+		{
+			auto Input = DowncastedSource.mInputs[i];
+			if (Input)
+			{
+				UE_LOG(LogAutoSplitters,Display,TEXT("Source input %d: %s"),i,Input->IsConnected() ? TEXT("Connected") : TEXT("Not connected"));
+				if (Input->IsConnected())
+				{
+					UE_LOG(LogAutoSplitters,Display,TEXT("Connected source input %d : %d, connected: %s"),i,Input->GetConnection(),Input->GetConnection()->IsConnected());
+					if (Input->GetConnection()->IsConnected())
+					{
+						UE_LOG(LogAutoSplitters,Display,TEXT("Connected source input %d : connected to: %s"),i,Input->GetConnection()->GetConnection());					
+					}
+				}
+			}
+		}
+
+		for (int32 i = 0 ; i < DowncastedSource.mOutputs.Num() ; ++i)
+		{
+			auto Output = DowncastedSource.mOutputs[i];
+			if (Output)
+			{
+				UE_LOG(LogAutoSplitters,Display,TEXT("Source output %d: %s"),i,Output->IsConnected() ? TEXT("Connected") : TEXT("Not connected"));
+				if (Output->IsConnected())
+				{
+					UE_LOG(LogAutoSplitters,Display,TEXT("Connected source output %d : %d, connected: %s"),i,Output->GetConnection(),Output->GetConnection()->IsConnected());
+					if (Output->GetConnection()->IsConnected())
+					{
+						UE_LOG(LogAutoSplitters,Display,TEXT("Connected source output %d : connected to: %s"),i,Output->GetConnection()->GetConnection());					
+					}
+				}
+			}
+		}
+		*/
+		Call.Cancel();
+
+		/*
+		UE_LOG(LogAutoSplitters,Display,TEXT("Entering original PreUpgrade() implementation"));
+		Call(self);
+		UE_LOG(LogAutoSplitters,Display,TEXT("Returned from original PreUpgrade() implementation"));
+
+		for (int32 i = 0 ; i < DowncastedSource.mInputs.Num() ; ++i)
+		{
+			auto Input = DowncastedSource.mInputs[i];
+			if (Input)
+			{
+				UE_LOG(LogAutoSplitters,Display,TEXT("Source input %d: %s"),i,Input->IsConnected() ? TEXT("Connected") : TEXT("Not connected"));
+				if (Input->IsConnected())
+				{
+					UE_LOG(LogAutoSplitters,Display,TEXT("Connected source input %d : %d, connected: %s"),i,Input->GetConnection(),Input->GetConnection()->IsConnected());
+					if (Input->GetConnection()->IsConnected())
+					{
+						UE_LOG(LogAutoSplitters,Display,TEXT("Connected source input %d : connected to: %s"),i,Input->GetConnection()->GetConnection());					
+					}
+				}
+			}
+		}
+
+		for (int32 i = 0 ; i < DowncastedSource.mOutputs.Num() ; ++i)
+		{
+			auto Output = DowncastedSource.mOutputs[i];
+			if (Output)
+			{
+				UE_LOG(LogAutoSplitters,Display,TEXT("Source output %d: %s"),i,Output->IsConnected() ? TEXT("Connected") : TEXT("Not connected"));
+				if (Output->IsConnected())
+				{
+					UE_LOG(LogAutoSplitters,Display,TEXT("Connected source output %d : %d, connected: %s"),i,Output->GetConnection(),Output->GetConnection()->IsConnected());
+					if (Output->GetConnection()->IsConnected())
+					{
+						UE_LOG(LogAutoSplitters,Display,TEXT("Connected source output %d : connected to: %s"),i,Output->GetConnection()->GetConnection());					
+					}
+				}
+			}
+		}
+		*/
+
+	};
+	
+	//SUBSCRIBE_METHOD(IFGDismantleInterface::Execute_PreUpgrade,PreUpgradeHook);
+
+
+	
+	
+	auto UpgradeHook = [](auto& Call, UObject* self, AActor* newActor)
 	{
-		UE_LOG(LogAutoSplitters,Display,TEXT("Hook was hit"));
-		Call(self,newActor);
-	});
-	*/
+
+		UE_LOG(LogAutoSplitters,Display,TEXT("Entered hook for IFGDismantleInterface::Execute_Upgrade()"));
+	
+		AMFGBuildableAutoSplitter* Target = Cast<AMFGBuildableAutoSplitter>(newActor);
+		if (!Target)
+		{
+			UE_LOG(LogAutoSplitters,Display,TEXT("Target is not an AMFGBuildableAutoSplitter, bailing out"));
+			return;
+		}
+		AFGBuildableAttachmentSplitter* Source = Cast<AFGBuildableAttachmentSplitter>(self);
+		if (!Source)
+		{
+			UE_LOG(LogAutoSplitters,Display,TEXT("self is not an AFGBuildableAttachmentSplitter, bailing out"));
+			return;
+		}
+
+		//UE_LOG(LogAutoSplitters,Display,TEXT("Calling UpgradeFromSplitter()"));
+		//Target->UpgradeFromSplitter(*Source);
+
+		UE_LOG(LogAutoSplitters,Display,TEXT("Cancelling original call"));
+		Call.Cancel();
+	};
+	
+	SUBSCRIBE_METHOD(IFGDismantleInterface::Execute_Upgrade,UpgradeHook);
 
 #endif // UE_BUILD_SHIPPING
 
