c4bcee4e8022d847475d325663972e52f7b82807
Steffen Müthing <steffen.muething@posteo.de>
2021-06-01 13:26:58 +0200

    Improve warning message for conveyor removal

--- Plugins/AutoSplitters/Source/AutoSplitters/Private/AutoSplittersModule.cpp
+++ Plugins/AutoSplitters/Source/AutoSplitters/Private/AutoSplittersModule.cpp
@@ -53,9 +53,12 @@ void FAutoSplittersModule::StartupModule()
 
 		if (this->mDismantledConveyors > 0)
 		{
-			UE_LOG(LogAutoSplitters,Warning,TEXT("Dismantled %d conveyors during AutoSplitter upgrade"),mDismantledConveyors);
+			UE_LOG(LogAutoSplitters,Warning,TEXT("AutoSplitters Mod Upgrade: Dismantled %d Conveyors"),mDismantledConveyors);
 
-			FString Str = FString::Printf(TEXT("Your savegame contained Auto Splitters created with versions of the mod older than 0.3.0. %d Auto Splitters were upgraded."),mUpgradedSplitters);
+			FString Str = FString::Printf(TEXT("Your savegame contained Auto Splitters created with versions of the mod older than 0.3.0,"\
+				"which connect to the attached conveyors in a wrong way. The mod has upgraded these Auto Splitters, but some connections could"\
+				"not be repaired.\n\n WARNING: Those conveyors have been dismantled to make it easy for you to find the broken splitters.\n\nA total "\
+				"of %d conveyors have been removed."),mDismantledConveyors);
 
 			AFGPlayerController* LocalController = nullptr;
 			for (auto Iterator = WorldSettings->GetWorld()->GetPlayerControllerIterator() ; Iterator ; ++Iterator)
@@ -68,7 +71,7 @@ void FAutoSplittersModule::StartupModule()
 
 			FPopupClosed CloseDelegate;
 
-			UFGBlueprintFunctionLibrary::AddPopupWithCloseDelegate(LocalController,FText::FromString("Savegame upgraded to AutoSplitters 0.3.0+"),FText::FromString(Str),CloseDelegate);
+			UFGBlueprintFunctionLibrary::AddPopupWithCloseDelegate(LocalController,FText::FromString("Savegame upgraded to AutoSplitters 0.3.x"),FText::FromString(Str),CloseDelegate);
 		}
 
 		mUpgradedSplitters = 0;
