7ee148bd83851bab66762affd795d3c62c07c89f
Steffen Müthing <steffen.muething@posteo.de>
2021-06-01 02:42:51 +0200

    Add InitGameInstance class

---
+++ Plugins/AutoSplitters/Source/AutoSplitters/Private/AutoSplittersInitGameInstance.cpp
@@ -0,0 +1,28 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AutoSplittersInitGameInstance.h"
+
+#include "Registry/RemoteCallObjectRegistry.h"
+#include "AutoSplittersLog.h"
+#include "AutoSplittersRCO.h"
+
+UAutoSplittersInitGameInstance::UAutoSplittersInitGameInstance()
+{
+    bRootModule = true;
+}
+
+void UAutoSplittersInitGameInstance::DispatchLifecycleEvent(ELifecyclePhase Phase)
+{
+    switch(Phase)
+    {
+        case ELifecyclePhase::CONSTRUCTION:
+            {
+                UE_LOG(LogAutoSplitters,Display,TEXT("Registering AutoSplittersRCO object with RemoteCallObjectRegistry"));
+                auto RCORegistry = GetGameInstance()->GetSubsystem<URemoteCallObjectRegistry>();
+                RCORegistry->RegisterRemoteCallObject(UAutoSplittersRCO::StaticClass());
+            }
+        default:
+            break;
+    }
+}
---
+++ Plugins/AutoSplitters/Source/AutoSplitters/Public/AutoSplittersInitGameInstance.h
@@ -0,0 +1,24 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+#include "Module/GameInstanceModule.h"
+
+#include "AutoSplittersInitGameInstance.generated.h"
+
+/**
+ *
+ */
+UCLASS(BlueprintType)
+class AUTOSPLITTERS_API UAutoSplittersInitGameInstance : public UGameInstanceModule
+{
+    GENERATED_BODY()
+
+public:
+
+    UAutoSplittersInitGameInstance();
+
+    virtual void DispatchLifecycleEvent(ELifecyclePhase Phase) override;
+};
