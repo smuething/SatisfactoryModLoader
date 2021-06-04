9d61a83aa2efbfa7b30ae6c07100065f0aed6255
Steffen Müthing <steffen.muething@posteo.de>
2021-06-05 01:46:00 +0200

    Add subsystem holding saved version and mod config

---
+++ Plugins/AutoSplitters/Source/AutoSplitters/Private/Subsystem/AutoSplittersSubsystem.cpp
@@ -0,0 +1,39 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Subsystem/AutoSplittersSubsystem.h"
+
+AAutoSplittersSubsystem* AAutoSplittersSubsystem::mCachedSubsystem = nullptr;
+
+AAutoSplittersSubsystem::AAutoSplittersSubsystem()
+{
+    ReplicationPolicy = ESubsystemReplicationPolicy::SpawnOnServer;
+}
+
+void AAutoSplittersSubsystem::Init()
+{
+    Super::Init();
+    ReloadConfig();
+}
+
+void AAutoSplittersSubsystem::EndPlay(const EEndPlayReason::Type EndPlayReason)
+{
+    Super::EndPlay(EndPlayReason);
+    if (this == mCachedSubsystem)
+        mCachedSubsystem = nullptr;
+}
+
+void AAutoSplittersSubsystem::ReloadConfig()
+{
+    mConfig = FAutoSplitters_ConfigStruct::GetActiveConfig();
+}
+
+/*
+void AAutoSplittersSubsystem::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+    Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+    DOREPLIFETIME(AAutoSplittersSubsystem,mMaxSplitterVersion);
+    DOREPLIFETIME(AAutoSplittersSubsystem,mMinSplitterVersion);
+    DOREPLIFETIME(AAutoSplittersSubsystem,mConfig);
+}
+*/
--- Plugins/AutoSplitters/Source/AutoSplitters/Public/Subsystem/AutoSplittersSubsystem.h
+++ Plugins/AutoSplitters/Source/AutoSplitters/Public/Subsystem/AutoSplittersSubsystem.h
@@ -3,14 +3,57 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "UObject/Object.h"
+#include "Subsystem/ModSubsystem.h"
+#include "Buildables/MFGBuildableAutoSplitter.h"
+#include "AutoSplitters_ConfigStruct.h"
+#include "Subsystem/SubsystemActorManager.h"
+#include "AutoSplittersLog.h"
+
 #include "AutoSplittersSubsystem.generated.h"
 
 /**
- * 
+ *
  */
 UCLASS()
-class AUTOSPLITTERS_API UAutoSplittersSubsystem : public UObject
+class AUTOSPLITTERS_API AAutoSplittersSubsystem : public AModSubsystem
 {
     GENERATED_BODY()
+
+    friend class FAutoSplittersModule;
+
+    static AAutoSplittersSubsystem* mCachedSubsystem;
+
+public:
+
+    UPROPERTY(SaveGame,BlueprintReadOnly)
+    uint8 mMaxSplitterVersion = AMFGBuildableAutoSplitter::VERSION;
+
+    UPROPERTY(SaveGame,BlueprintReadOnly)
+    uint8 mMinSplitterVersion = 0;
+
+    UPROPERTY(Transient,BlueprintReadOnly)
+    FAutoSplitters_ConfigStruct mConfig;
+
+protected:
+
+    virtual void Init() override;
+    virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
+
+public:
+
+    AAutoSplittersSubsystem();
+
+    static AAutoSplittersSubsystem* Get(UObject* WorldContext)
+    {
+        if (mCachedSubsystem)
+            return mCachedSubsystem;
+        const auto World = WorldContext->GetWorld();
+        auto SubsystemActorManager = World->GetSubsystem<USubsystemActorManager>();
+        mCachedSubsystem = SubsystemActorManager->GetSubsystemActor<AAutoSplittersSubsystem>();
+        check(mCachedSubsystem);
+        return mCachedSubsystem;
+    }
+
+    void ReloadConfig();
+
 };
